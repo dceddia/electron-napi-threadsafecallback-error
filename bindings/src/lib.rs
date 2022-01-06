d80eb0da7fae38900e258e45fedb564b98b80cee
Dave Ceddia <dave@daveceddia.com>
2022-01-06 10:29:14 -0500

    2 threads cause deadlock (panic)
    It's not actually a deadlock but that's only because uv_lock_mutex is
    calling abort() instead of letting the deadlock happen.

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -23,43 +23,26 @@ pub fn sum(a: i32, b: i32) -> i32 {
 
 #[napi]
 pub struct JsRepeater {
-  handle: Option<JoinHandle<Result<()>>>,
+  handle1: Option<JoinHandle<Result<()>>>,
+  handle2: Option<JoinHandle<Result<()>>>,
   quit: Arc<AtomicBool>,
 }
 
 #[napi]
 impl JsRepeater {
   #[napi(constructor)]
-  pub fn new(env: Env, callback: JsFunction) -> Result<Self> {
+  pub fn new(callback: JsFunction) -> Result<Self> {
     // Create a threadsafe function around the given callback
-    let mut ts_callback: ThreadsafeFunction<u32> =
-      callback.create_threadsafe_function(0, send_update)?;
-    ts_callback.refer(&env).expect("failed to ref the callback");
+    let cb1: ThreadsafeFunction<u32> = callback.create_threadsafe_function(0, send_update)?;
+    let cb2: ThreadsafeFunction<u32> = callback.create_threadsafe_function(0, send_update)?;
 
     let quit = Arc::new(AtomicBool::new(false));
-    let should_quit = quit.clone();
-    let handle = thread::spawn(move || {
-      let mut i = 0;
-      while !should_quit.load(Ordering::SeqCst) {
-        thread::sleep(Duration::from_millis(500));
-        let status = ts_callback.call(
-          Ok(i),
-          napi::threadsafe_function::ThreadsafeFunctionCallMode::NonBlocking,
-        );
-        println!("called function with {}, {}", i, status);
-        i += 1;
-      }
-      println!(
-        "done with loop, callback is aborted? {:?}",
-        ts_callback.aborted()
-      );
-      // ts_callback.abort().expect("failed to abort");
-
-      Ok(())
-    });
+    let handle1 = spawn_worker("ONE".to_string(), quit.clone(), cb1);
+    let handle2 = spawn_worker("TWO".to_string(), quit.clone(), cb2);
 
     Ok(JsRepeater {
-      handle: Some(handle),
+      handle1: Some(handle1),
+      handle2: Some(handle2),
       quit,
     })
   }
@@ -69,14 +52,47 @@ impl Drop for JsRepeater {
   fn drop(&mut self) {
     self.quit.store(true, Ordering::SeqCst);
 
-    if let Some(thread) = self.handle.take() {
-      println!("joining Repeater thread...");
+    if let Some(thread) = self.handle1.take() {
+      println!("joining handle1...");
       let _ = thread.join();
-      println!("joined");
+      println!("joined handle1");
+    }
+
+    if let Some(thread) = self.handle2.take() {
+      println!("joining handle...");
+      let _ = thread.join();
+      println!("joined handle2");
     }
   }
 }
 
 fn send_update(ctx: ThreadSafeCallContext<u32>) -> Result<Vec<JsNumber>> {
   ctx.env.create_uint32(ctx.value).map(|v| vec![v])
+}
+
+fn spawn_worker(
+  name: String,
+  should_quit: Arc<AtomicBool>,
+  ts_callback: ThreadsafeFunction<u32>,
+) -> JoinHandle<Result<()>> {
+  thread::spawn(move || {
+    let mut i = 0;
+
+    while !should_quit.load(Ordering::SeqCst) {
+      thread::sleep(Duration::from_millis(500));
+      let status = ts_callback.call(
+        Ok(i),
+        napi::threadsafe_function::ThreadsafeFunctionCallMode::NonBlocking,
+      );
+      println!("called function {} with {}, {}", name, i, status);
+      i += 1;
+    }
+
+    println!(
+      "done with loop, callback is aborted? {:?}",
+      ts_callback.aborted()
+    );
+
+    Ok(())
+  })
 }
